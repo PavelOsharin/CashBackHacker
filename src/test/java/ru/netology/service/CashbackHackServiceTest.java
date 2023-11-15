d6fe21b12a518822a69d2e53f08894d530b82aa7
Pavel Osharin <mr.osharin@mail.ru>
2023-11-15 13:32:47 +0300

    Add test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void shouldAddAmountIfLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldAddAmountIfMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int expected = 800;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldNotAddAmountIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+}
